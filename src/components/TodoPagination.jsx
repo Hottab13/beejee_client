3e76a3058e25891c5b7ea9ce83a3e20f72378faf
Hottab13 <alekseev.a@hotmail.com>
2023-04-20 11:42:23 +0300

    0_5

--- src/components/TodoPagination.jsx
+++ src/components/TodoPagination.jsx
@@ -1,6 +1,49 @@
 import { useDispatch } from "react-redux";
 import { getAllTodos } from "../store/todoSlice";
 
+const NextBtn = ({ dispatch, page }) => (
+  <li>
+    <button
+      onClick={() => dispatch(getAllTodos({ page: page + 1 }))}
+      className="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 rounded-r-lg leading-tight py-2 px-3"
+    >
+      Вперед
+    </button>
+  </li>
+);
+const BackBtn = ({ dispatch, page }) => (
+  <li>
+    <button
+      onClick={() => dispatch(getAllTodos({ page: page - 1 }))}
+      className="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 ml-0 rounded-l-lg leading-tight py-2 px-3 "
+    >
+      Назад
+    </button>
+  </li>
+);
+const ElItemPage = ({ dispatch, el, page }) => (
+  <li>
+    <button
+      onClick={() => dispatch(getAllTodos({ page: el }))}
+      className={
+        page === el
+          ? "bg-blue-50 border border-gray-300 text-blue-600 hover:bg-blue-100 hover:text-blue-700  py-2 px-3 "
+          : "bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 ml-0  leading-tight py-2 px-3"
+      }
+    >
+      {el}
+    </button>
+  </li>
+);
+const PaginationContainer = ({ children }) => (
+  <nav className=" flex justify-center">
+    <ul className="flex flex-warp  -space-x-px my-4 rounded-lg">{children}</ul>
+  </nav>
+);
+const TotalPages = ({ children }) => (
+  <span className="pl-4">Страниц: {children}</span>
+);
+
 const TodoPagination = ({ totalPages, page }) => {
   const dispatch = useDispatch();
   const pageNumbers = [];
@@ -8,43 +51,14 @@ const TodoPagination = ({ totalPages, page }) => {
     pageNumbers.push(i);
   }
   return (
-    <nav className=" flex justify-center">
-      <ul className="flex flex-warp  -space-x-px my-4 rounded-lg">
-        <li>
-          <button
-            onClick={() => dispatch(getAllTodos({ page: page - 1 }))}
-            className="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 ml-0 rounded-l-lg leading-tight py-2 px-3 "
-          >
-            Назад
-          </button>
-        </li>
-        {pageNumbers.map((el) => (
-          <li key={el}>
-            <button
-              onClick={() => dispatch(getAllTodos({ page: el }))}
-              className={
-                page === el
-                  ? "bg-blue-50 border border-gray-300 text-blue-600 hover:bg-blue-100 hover:text-blue-700  py-2 px-3 "
-                  : "bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 ml-0  leading-tight py-2 px-3"
-              }
-            >
-              {el}
-            </button>
-          </li>
-        ))}
-        {page < totalPages && (
-          <li>
-            <button
-              onClick={() => dispatch(getAllTodos({ page: page + 1 }))}
-              className="bg-white border border-gray-300 text-gray-500 hover:bg-gray-100 hover:text-gray-700 rounded-r-lg leading-tight py-2 px-3"
-            >
-              Вперед
-            </button>
-          </li>
-        )}
-        <span className="pl-4">Страниц: {totalPages}</span>
-      </ul>
-    </nav>
+    <PaginationContainer>
+      <BackBtn dispatch={dispatch} page={page} />
+      {pageNumbers.map((el) => (
+        <ElItemPage dispatch={dispatch} el={el} page={page} key={el} />
+      ))}
+      {page < totalPages && <NextBtn dispatch={dispatch} page={page} />}
+      <TotalPages>{totalPages}</TotalPages>
+    </PaginationContainer>
   );
 };
 export { TodoPagination };
